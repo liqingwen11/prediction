a093b5d20fd943b5f4a2280a8a987966a2ef3c6c
liqingwen11 <liqingwen@sjtu.edu.cn>
2024-07-27 12:37:59 +0800

    Create data_preprocessing.py

---
+++ utils/data_preprocessing.py
@@ -0,0 +1,35 @@
+from torchvision import transforms
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+
+train_transforms = transforms.Compose([
+    transforms.RandomResizedCrop(128),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+val_transforms = transforms.Compose([
+    transforms.Resize(128),
+    transforms.CenterCrop(128),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+class CustomDataset(Dataset):
+    def __init__(self, image_paths, labels, transform=None):
+        self.image_paths = image_paths
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image = Image.open(self.image_paths[idx])
+        label = self.labels[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
